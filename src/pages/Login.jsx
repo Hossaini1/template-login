01150eec35d1cfbb868e7b79d6c52c3aadcd575d
Hossaini <1989hossaini1@gmail.com>
2024-03-16 14:08:55 +0100

    login formular template added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,57 @@
+import { useState } from "react"
+
 function Login() {
+  const [formData, setFormData]= useState({username:'',password:''});
+
+  //  alternative 
+  // const [username, setUsername]=useState('');
+  // const [password, setPassword]=useState('');
+
+const handleInputChange = (e)=>{
+  setFormData({...formData, [e.target.name]:e.target.value})
+ 
+  // alternative
+  // const {name,value}=e.target;
+  // setFormData({...formData,[name]: value})
+};
+
+const handleSubmit = (e)=>{
+  e.preventDefault()
+  console.log('Form Data', formData);
+}
   return (
-    <div>Login</div>
+
+    <form onSubmit={handleSubmit}>
+       <div>
+        <label htmlFor="userName">Username: </label>
+        <input 
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleInputChange}
+        id="userName"
+        placeholder="Username"
+        autoComplete=""
+        required
+         />
+       </div>
+
+       <div>
+        <label htmlFor="Password">Password: </label>
+        <input 
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleInputChange}
+        id="Password"
+        placeholder="Password"
+        autoComplete=""
+        required
+         />
+       </div>
+       <button type="submit">Login</button>
+    </form>
+   
   )
 }
 export default Login
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom"
 
+
+
+
 function NotFoundPage() {
   const navigate = useNavigate()
   const handleClick = ()=>{
@@ -7,6 +10,9 @@ function NotFoundPage() {
   }
   return (
     <div>
+      
+
+
       <h2>ERROR 404 , die Page wurde nicht gefunden!</h2>
       <button onClick={handleClick}>Zurück zue Startseite</button>
     </div>
